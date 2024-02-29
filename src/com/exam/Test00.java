ca743db71808a8e971ea70308f7403cea4300223
YungukKim <kug1328@naver.com>
2024-02-29 09:33:08 +0900

    third commit 02 수정, Test01 삭제

--- src/com/exam/Test00.java
+++ src/com/exam/Test00.java
@@ -1,5 +1,7 @@
 package com.exam;
 
 public class Test00 {
-	
+	void aa() {
+		System.out.println("두 번째 커밋 수정");
+	}
 }
--- src/com/exam/Test02.java
+++ src/com/exam/Test02.java
@@ -1,5 +1,7 @@
 package com.exam;
 
 public class Test02 {
-
+	void select() {
+		System.out.println("세 번째 커밋 수정");
+	}
 }
