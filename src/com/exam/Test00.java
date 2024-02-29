c67e78aa32bf2a098ba36a8a40f2273826b088c9
YungukKim <kug1328@naver.com>
2024-02-29 10:41:50 +0900

    Test 00 수정, Test06 추가

--- src/com/exam/Test00.java
+++ src/com/exam/Test00.java
@@ -4,7 +4,7 @@ public class Test00 {
 	void aa() {
 		System.out.println("두 번째 커밋 수정");
 	}
-	void bb() {
-		System.out.println("Cart에서 추가함");
+	void cc() {
+		System.out.println("main에서 추가함");
 	}
 }
