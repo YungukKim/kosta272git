db6d75392a2af2accf468e6a8c607e6cdb99f544
YungukKim <kug1328@naver.com>
2024-02-29 10:47:03 +0900

    Test 00 수정

--- src/com/exam/Test00.java
+++ src/com/exam/Test00.java
@@ -4,6 +4,9 @@ public class Test00 {
 	void aa() {
 		System.out.println("두 번째 커밋 수정");
 	}
+	void bb() {
+		System.out.println("cart에서 추가함");
+	}
 	void cc() {
 		System.out.println("main에서 추가함");
 	}
